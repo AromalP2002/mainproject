c6f3e78408f41919e8a80f7f2062f2396292b249
AromalP2002 <akhilaromal938@gmail.com>
2025-02-14 11:29:50 +0530

    well

--- bookingpage/app/models.py
+++ bookingpage/app/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class booking(models.Model):
+ 
     
+  
+    specialization = models.CharField(max_length=100)
+    available = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f"Dr. {self.user.get_full_name()} ({self.specialization})"
+
+
+      user = models.OneToOneField(User, on_delete=models.CASCADE)
