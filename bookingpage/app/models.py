dfc1b38c236a3f28a3d305542b8fd4886017f3cb
AromalP2002 <akhilaromal938@gmail.com>
2025-02-18 11:14:12 +0530

    well

--- bookingpage/app/models.py
+++ bookingpage/app/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class booking(models.Model):
+# Create your models here.
+# class booking(models.Model):
+
  
     
   
-    specialization = models.CharField(max_length=100)
-    available = models.BooleanField(default=True)
     
-    def __str__(self):
-        return f"Dr. {self.user.get_full_name()} ({self.specialization})"
 
-
-     
